310be8bb92850629dd03c41a828ab1bd91342faf
anisha-perpule <31889341+anisha98@users.noreply.github.com>
2020-01-21 16:10:46 +0530

    SignUp Activity added

---
+++ app/src/main/java/com/example/perpuleapptest/ui/auth/SignUpActivity.kt
@@ -0,0 +1,51 @@
+package com.example.perpuleapptest.ui.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.example.perpuleapptest.R
+import com.example.perpuleapptest.databinding.ActivitySignUpBinding
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
+
+class SignupActivity : AppCompatActivity(), SignUpListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val binding: ActivitySignUpBinding
+                = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        val viewModel = ViewModelProviders.of(this).get(SignUpViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+
+        //viewModel.getLoggedInUser().observe(this, Observer { user ->
+            //if(user != null){
+             //   Intent(this, HomeActivity::class.java).also {
+                //    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+              //      startActivity(it)
+            //    }
+          //  }
+        //})
+
+    }
+
+
+    override fun onStarted() {
+        Toast.makeText(this,"Started", Toast.LENGTH_SHORT)
+    }
+
+    override fun onSuccess() {
+        Toast.makeText(this,"Success", Toast.LENGTH_SHORT)
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this,message, Toast.LENGTH_SHORT)
+    }
+}
---
+++ app/src/main/java/com/example/perpuleapptest/ui/auth/SignUpViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.perpuleapptest.ui.auth
+
+import android.content.Intent
+import android.view.View
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    var email: String? = null
+    var password: String? = null
+    var confirm: String? = null
+
+    var authListener: SignUpListener? = null
+
+    fun home(view: View) {
+        authListener?.onStarted()
+        if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
+            authListener?.onFailure("Invalid email or password")
+            return
+        }
+        authListener?.onStarted()
+
+    }
+}
