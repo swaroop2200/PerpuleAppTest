9c6a895626ef7ea494c3808ad5533871b968a757
swaroop2200 <31801645+swaroop2200@users.noreply.github.com>
2020-01-21 13:37:29 +0530

    added loginactivity and its xml

---
+++ app/src/main/java/com/example/perpuleapptest/ui/auth/LoginActitvity.kt
@@ -0,0 +1,13 @@
+package com.example.perpuleapptest.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.perpuleapptest.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/perpuleapptest/ui/auth/LoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.perpuleapptest.ui.auth
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel:ViewModel() {
+
+    var name:String?=null;
+    var email:String?=null;
+
+}
