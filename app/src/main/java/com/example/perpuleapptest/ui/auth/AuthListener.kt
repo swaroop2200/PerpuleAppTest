b016f719d1651f9a3c322c524636c041b530d27b
swaroop2200 <31801645+swaroop2200@users.noreply.github.com>
2020-01-21 15:56:49 +0530

    Added AuthListener, LoginActivity and LoginView Model just tested data binding with toast

---
+++ app/src/main/java/com/example/perpuleapptest/ui/auth/AuthListener.kt
@@ -0,0 +1,7 @@
+package com.example.perpuleapptest.ui.auth
+
+interface AuthListener {
+    fun onStarted()
+    fun onSuccess()
+    fun onFailure()
+}
--- app/src/main/java/com/example/perpuleapptest/ui/auth/LoginViewModel.kt
+++ app/src/main/java/com/example/perpuleapptest/ui/auth/LoginViewModel.kt
@@ -1,10 +1,21 @@
 package com.example.perpuleapptest.ui.auth
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 
-class LoginViewModel:ViewModel() {
+class LoginViewModel:ViewModel(){
 
-    var name:String?=null;
-    var email:String?=null;
+    var email: String? = null;
+    var password: String? = null;
 
+    var authListener: AuthListener? = null
+
+    fun onLoginButtonClick(view: View) {
+        authListener?.onStarted()
+        if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
+            authListener?.onFailure()
+            return
+        }
+
+    }
 }
